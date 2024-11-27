f45980c0d25616237608f9701ed275a77533fd1a
karthik <prudhvi>
2024-11-27 11:24:28 +0530

    msg

--- src/com/gitbasedprojects/Firstprogram.java
+++ src/com/gitbasedprojects/Firstprogram.java
@@ -2,6 +2,8 @@ package com.gitbasedprojects;
 
 public class Firstprogram {
 public static void main(String[] args) {
+
+	System.out.println("enter a program");
 	System.out.println("enter a program");
 }
 }
