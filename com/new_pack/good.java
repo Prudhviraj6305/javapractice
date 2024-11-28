d594d1a171f21eca431f9883bfd0d129bda21d37
karthik <prudhvi>
2024-11-28 14:36:34 +0530

    this is new java based program

--- com/new_pack/good.java
+++ com/new_pack/good.java
@@ -2,7 +2,28 @@ package com.new_pack;
 
 public class good {
 public static void main(String[] args) {
-	System.out.println("the ending message ");
-	System.out.println("the ending message for now");
+	int arr[]= {10,15,20,20};
+	frequency(arr);
 }
+
+private static void frequency(int arr[]) {
+	boolean b[]= new boolean[arr.length];
+	int count=1;
+		for(int i=0;i<arr.length;i++)
+		{
+			if(!b[i])
+			{
+			for(int j=i+1;j<arr.length;j++) {
+			if(arr[i]==arr[j])
+			{
+				count++;
+				b[j]=true;
+			}
+			
+		}
+			System.out.println(arr[i]+"="+count);
+
+}
+
+}}
 }
