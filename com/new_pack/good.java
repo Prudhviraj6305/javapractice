4baf1c253df58f7452fd3f46d39edd8856573a35
karthik <prudhvi>
2024-11-28 12:10:40 +0530

    this is an end messange

---
+++ com/new_pack/good.java
@@ -0,0 +1,8 @@
+package com.new_pack;
+
+public class good {
+public static void main(String[] args) {
+	System.out.println("the ending message ");
+	System.out.println("the ending message for now");
+}
+}
